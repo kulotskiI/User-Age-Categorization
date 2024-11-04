8752fa3fe73abacaedac83ac3b1dbabbb18210de
kulotskiI <ishielmishma24@gmail.com>
2024-11-04 20:06:56 +0800

    initial commit

---
+++ userAge.js
@@ -0,0 +1,19 @@
+let age = 20;
+if (age < 5){
+console.log("You are an Toddler. ");
+}
+else if (age <= 60 && age >=32){
+    console.log("You are at the Middle-Aged")
+}
+else if (age <=35 && age >= 20){
+    console.log("You are an Young Adult")
+}
+else if (age <= 13 && age >=19){
+    console.log("You are Teenager")
+}
+else if (age >=12 && age <=5){
+    console.log("You are an Child")
+}
+else if (age > 60) {
+    console.log("You are a Senior.");
+}
